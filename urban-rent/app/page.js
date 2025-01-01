5d10ac99be2145060b69ef686f586c220c087c69
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-01-01 17:18:53 +0300

    get lined

--- urban-rent/app/page.js
+++ urban-rent/app/page.js
@@ -1,24 +1,11 @@
-import Image from "next/image";
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
-import { faCat } from '@awesome.me/kit-KIT_CODE/icons/sharp/solid'
-import { faDog } from '@awesome.me/kit-KIT_CODE/icons/duotone/solid'
-import { faDragon } from '@awesome.me/kit-KIT_CODE/icons/sharp-duotone/solid'
+import React from 'react'
 
-const element = (
-  <div>
-    <FontAwesomeIcon icon={faHouse} />
-    <FontAwesomeIcon icon={faCat} />
-    <FontAwesomeIcon icon={faDog} />
-    <FontAwesomeIcon icon={faDragon} />
-  </div>
-)
-
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <h1>Urban Rent</h1>
     </div>
-  );
+  )
 }
+
+export default Home
